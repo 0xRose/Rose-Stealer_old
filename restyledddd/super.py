dc986cf341bb8c009dd2da196593c933aded7a0d
Restyled.io <commits@restyled.io>
2023-03-22 12:35:45 +0000

    Restyled by black

--- restyledddd/super.py
+++ restyledddd/super.py
@@ -8,7 +8,8 @@ import colorama
 import requests
 import subprocess
 import os
-os.system('cls')
+
+os.system("cls")
 
 
 init(convert=True)
@@ -20,19 +21,23 @@ def program():
 
 def connect():
     URL = "Your Pastebin RAW URL here"
-    hwid = subprocess.check_output(
-        'wmic csproduct get uuid').decode().split('\n')[1].strip()
+    hwid = (
+        subprocess.check_output("wmic csproduct get uuid")
+        .decode()
+        .split("\n")[1]
+        .strip()
+    )
 
     rq = requests.get(url)
 
     if rq.status_code == 404:
-        print(f'{Fore.LIGHTRED_EX}The URL provided is invalid')
+        print(f"{Fore.LIGHTRED_EX}The URL provided is invalid")
         return
     if hwid in rq.text:
         program()
     else:
-        print(f'{Fore.LIGHTRED_EX}Your HWID isn\'t in the database')
-        print(f'{Fore.LIGHTRED_EX}Your HWID: {Fore.LIGHTYELLOW_EX}{hwid}')
+        print(f"{Fore.LIGHTRED_EX}Your HWID isn't in the database")
+        print(f"{Fore.LIGHTRED_EX}Your HWID: {Fore.LIGHTYELLOW_EX}{hwid}")
 
 
 connect()
