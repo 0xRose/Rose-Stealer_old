96642cafa756f60d6500be93e19510e933f7a19f
Silky <96620548+S1LKT0UCH@users.noreply.github.com>
2023-03-27 20:44:17 +0200

    Update launch_builder.bat

--- launch_builder.bat
+++ launch_builder.bat
@@ -68,12 +68,12 @@ exit
 
 :NEW
 echo Starting the new builder now...
+echo WARNING | The result of closing this terminal is that the builder is also being exited.
 cd roseui
 python v8.py
-echo WARNING | The result of closing this terminal is that the builder is also being exited.
 
 :OLD
 echo Starting the old builder now...
+echo WARNING | The result of closing this terminal is that the builder is also being exited.
 cd tools
 python rose_builder.py
-echo WARNING | The result of closing this terminal is that the builder is also being exited.
