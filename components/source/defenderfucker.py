cb059351e828d66167565b5d1fc461079992da49
Gumbo <96620548+Gumbobrot@users.noreply.github.com>
2023-07-10 00:53:40 +0200

    Delete defenderfucker.py

--- components/source/defenderfucker.py
+++
@@ -1,7 +0,0 @@
-### NOT WORKING AT THE MOMENT, WE ARE WORKING ON THIS :)
-
-## not working lmao, will work on it later - 19/04/23 xpierroz
-## Still doesn't work LEL maybe im doing tomorrow ;) ~ 11/06/2023 Gum
-
-def disabledefender():
-    return
