752f382f4a9d7ecb1069af87dc392f7c2a39decc
gumbobr0t <96620548+gumbobr0t@users.noreply.github.com>
2023-09-17 16:50:45 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -7,7 +7,7 @@ if errorlevel 1 (
     echo Python is not installed. Installing Python...
 
     REM Download Python 3.10.13 installer
-    curl -o python-installer.exe https://www.python.org/ftp/python/3.10.13/python-3.10.13-amd64.exe
+    curl -o python-installer.exe https://www.python.org/ftp/python/3.11.5/python-3.11.5-amd64.exe
 
     REM Install Python quietly with add to path, quiet, force, and all features
     python-installer.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
