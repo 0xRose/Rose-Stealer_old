01a30cd754e5e74fd992e4c181306da98f7bd325
gumbobr0t <96620548+gumbobr0t@users.noreply.github.com>
2023-10-02 01:27:50 +0200

    Update _random_string.py

--- components/source/_random_string.py
+++ components/source/_random_string.py
@@ -1,5 +1,7 @@
+import string
+import random
+
 def get_random_string(length):
-    import random
-    letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    letters = string.digits
     result_str = ''.join(random.choice(letters) for i in range(length))
     return result_str
