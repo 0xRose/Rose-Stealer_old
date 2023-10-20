1fde7bd873942949b8280b970f44a64c882e3f70
gumbobr0t <96620548+gumbobr0t@users.noreply.github.com>
2023-10-20 19:05:50 +0200

    Update builder.bat

--- builder.bat
+++ builder.bat
@@ -1,59 +1,65 @@
-@echo off
+@echo off && color b
 setlocal EnableDelayedExpansion
 
-:: Step 1: Check if Python is installed
+REM Step 1: Check if Python is installed
 title Looking for Python...
-python --version >nul 2>&1
-if %errorlevel% neq 0 (
-    echo "Python is not installed. Installing..."
+python --version >nul 2>nul
+if errorlevel 1 (
+    echo Python is not installed. Installing...
     
-    :: Step 2: Install Python silently
+    REM Step 2: Install Python silently
     
-    :: Define variables
+    REM Define variables
     set "URL=https://www.python.org/ftp/python/3.11.5/python-3.11.5-amd64.exe"
     set "INSTALL_PATH=C:\Python311"
 
-    :: Download Python installer
+    REM Download Python installer
     curl -o "%TEMP%\python-3.11.5-amd64.exe" %URL%
 
-    :: Install Python silently
+    REM Install Python silently
     "%TEMP%\python-3.11.5-amd64.exe" /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 SimpleInstall=1 TargetDir=%INSTALL_PATH%
 
-    :: Add Python to system PATH
+    REM Add Python to system PATH
     setx PATH "%INSTALL_PATH%;%PATH%"
 
-    :: Clean up downloaded installer
+    REM Clean up downloaded installer
     del "%TEMP%\python-3.11.5-amd64.exe"
     
-    :: Close and reopen the console to apply PATH changes
+    REM Close and reopen the console to apply PATH changes
     exit
+) else (
+    echo Python is installed.
 )
 
-:: Step 3: Change console title
+REM Step 3: Change console title
 title Creating venv...
+echo Creating venv...
 
-:: Step 4: Create venv
+REM Step 4: Create venv
 python -m venv myvenv
 
-:: Step 5: Change console title
+REM Step 5: Change console title
 title Entering venv...
+echo Entering venv...
 
-:: Step 6: Activate venv
+REM Step 6: Activate venv
 call myvenv\Scripts\activate
 
-:: Step 7: Change console title
+REM Step 7: Change console title
 title Installing packages...
+echo Installing packages...
 
-:: Step 8: Install modules from requirements.txt
+REM Step 8: Install modules from requirements.txt
 pip install --upgrade --force-reinstall --ignore-installed --requirement resources\data\requirements.txt
 
-:: Step 9: Change console title
+REM Step 9: Change console title
 title Starting builder...
+echo Starting builder...
 
-:: Step 10: Run builder.py
+REM Step 10: Run builder.py
 title Rose UI Builder
 
 python resources\ui\builder.py
 
-:: Step 11: End local environment
+REM Step 11: End local environment
 endlocal
