cf4e04d1441d9ed1e91b10f4aa468f5a6d0e622e
gumbobr0t <96620548+gumbobr0t@users.noreply.github.com>
2023-12-19 22:43:33 +0100

    Delete resources/source/bin directory

--- resources/source/bin/_random_string.py
+++
@@ -1,7 +0,0 @@
-import string
-import random
-
-def get_random_string(length):
-    letters = string.digits
-    result_str = ''.join(random.choice(letters) for i in range(length))
-    return result_str
